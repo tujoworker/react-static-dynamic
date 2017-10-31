cd37437b9b1f1dd55b0c9aba4ab53f7946dcd38b
Tobias Høegh <tobias@tujo.no>
2017-10-31 10:52:45 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Router, Route, Switch, Link } from 'react-static'
+//
+import Home from 'containers/Home'
+import About from 'containers/About'
+import Dynamic from 'containers/Dynamic'
+import Blog from 'containers/Blog'
+import NotFound from 'containers/404'
+
+import './app.css'
+
+export default () => (
+    <Router>
+        <div>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/dynamic">Dynamic</Link>
+                <Link to="/blog">Blog</Link>
+            </nav>
+            <div className="content">
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/dynamic" component={Dynamic} />
+                    <Route path="/blog" component={Blog} />
+                    <Route component={NotFound} />
+                </Switch>
+            </div>
+        </div>
+    </Router>
+)
